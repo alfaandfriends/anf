575702df9eabeaa4b0be3719cdd4a402517e8ecd
Abdul Raof <abdulraof628@gmail.com>
2022-04-26 14:14:19 +0800

    add helpers

---
+++ app/Helpers.php
@@ -0,0 +1,9 @@
+<?php
+
+    function ad($array){
+        echo '<div style="padding-left: 260px; padding-top: 20px; font-size: 12px">';
+            echo '<pre>';
+            print_r($array);
+            echo '</pre>';
+        echo '</div>';
+    }
