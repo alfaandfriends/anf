a8381cb7a5a6a647746b46e58b1f5ab56eb31be0
JARRAKAL\Jarrakal <abdulroaf628@gmail.com>
2022-11-01 09:12:43 +0800

    fix student table

--- resources/js/Pages/CentreManagement/Classes/Index.vue
+++ resources/js/Pages/CentreManagement/Classes/Index.vue
@@ -146,7 +146,7 @@ import BreezeButton from '@/Components/Button.vue';
                                     </tr>
                                 </tbody>
                             </table>
-                            <template v-if="$page.props.classes.data.length">
+                            <template v-if="$page.props.classes.data">
                                 <div class="px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6 bg-gray-200">
                                     <div class="flex-1 flex justify-between sm:hidden">
                                         <a :href="$page.props.classes.prev_page_url" v-if="$page.props.classes.prev_page_url" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Previous </a>
