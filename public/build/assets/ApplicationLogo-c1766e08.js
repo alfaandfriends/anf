23c8f8c7de5801f6d1726feab204f556f079f76b
nazirul amin <lurizan23@gmail.com>
2023-04-20 12:03:38 +0800

    Build assets

--- public/build/assets/ApplicationLogo-c1766e08.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-c27b6911.js";import{o as r,h as n}from"./app-9e209d6c.js";const c={},s=["href"];function t(e,a){return r(),n("img",{href:e.route("dashboard"),src:"/images/anf-logo-main-2x.png",class:"w-35 h-35 block select-none",alt:""},null,8,s)}const _=o(c,[["render",t]]);export{_ as B};
