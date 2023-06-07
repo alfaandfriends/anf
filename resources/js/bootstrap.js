63c5fa98ed3810584e8b24821e777ed0c1ba43b5
nazirul amin <lurizan23@gmail.com>
2023-06-07 11:38:50 +0800

    Update import

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,13 +18,14 @@ axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
-// window.Pusher = require('pusher-js');
+window.Pusher = Pusher;
 // window.Pusher.logToConsole = true;
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true
+});
