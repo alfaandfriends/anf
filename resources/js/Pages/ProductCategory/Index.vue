c640093e124daf5f16f57aba261cd48e5133d010
nazirul amin <lurizan23@gmail.com>
2023-06-07 15:48:23 +0800

    Remove unnecessary comments

--- resources/js/Pages/ProductCategory/Index.vue
+++ resources/js/Pages/ProductCategory/Index.vue
@@ -3,11 +3,8 @@ import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import BreezeButton from '@/Components/Button.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 import { Inertia } from '@inertiajs/inertia'
-import ConfirmationModal from '@/Components/ConfirmationModal.vue';
 import Pagination from '@/Components/Pagination.vue';
 import { SearchIcon, TrashIcon, PencilIcon } from '@heroicons/vue/solid';
-import Multiselect from '@vueform/multiselect';
-import { $vfm, VueFinalModal, ModalsContainer } from 'vue-final-modal';
 
 const props = defineProps({
     categories: {
@@ -36,62 +33,6 @@ const handleDelete = (id) => {
                                     <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg focus:ring-0 focus:border-gray-300 appearance-none  block pl-10"
                                             type="text" v-model="params.search" placeholder="Search">
                                 </div> -->
-                                <div class="flex">
-                                    <!-- <Multiselect
-                                        v-model="params.centre_id"
-                                        valueProp="ID"
-                                        :appendNewOption="false"
-                                        :searchable="true"
-                                        :options="$page.props.allowed_centres"
-                                        :clearOnSelect="false"
-                                        :canClear="false"
-                                        :canDeselect="false"
-                                        trackBy="label"
-                                        label="label"
-                                        :classes="{
-                                            container:
-                                                $page.props.errors.centre_id ?
-                                                'relative mx-auto w-full flex items-center justify-end box-border cursor-pointer border-2 border-red-300 rounded-lg bg-white text-base leading-snug outline-none h-10 px-40 pr-16 ':
-                                                'relative mx-auto w-full flex items-center justify-end box-border cursor-pointer border-2 border-gray-300 rounded-lg bg-white text-base leading-snug outline-none h-10 px-40 pr-16 ',
-                                            containerDisabled: 'cursor-default bg-gray-100',
-                                            containerOpen: 'rounded-b-none',
-                                            containerOpenTop: 'rounded-t-none',
-                                            containerActive: 'border border-gray-300',
-                                            singleLabel: 'flex items-center h-full max-w-full absolute left-0 top-0 pointer-events-none bg-transparent leading-snug pl-3.5 pr-16 box-border',
-                                            singleLabelText: 'overflow-ellipsis overflow-hidden block whitespace-nowrap max-w-full',
-                                            multipleLabel: 'flex items-center h-full absolute left-0 top-0 pointer-events-none bg-transparent leading-snug pl-3.5',
-                                            search: 'w-full absolute inset-0 outline-none focus:ring-0 appearance-none box-border border-0 text-base font-sans bg-white rounded pl-3.5',
-                                            placeholder: 'flex items-center h-full absolute left-0 top-0 pointer-events-none bg-transparent leading-snug pl-3.5 text-gray-500',
-                                            caret: 'bg-multiselect-caret bg-center bg-no-repeat w-2.5 h-4 py-px box-content mr-3.5 relative z-10 opacity-40 flex-shrink-0 flex-grow-0 transition-transform transform pointer-events-none',
-                                            caretOpen: 'rotate-180 pointer-events-auto',
-                                            clear: 'pr-3.5 relative z-10 opacity-40 transition duration-300 flex-shrink-0 flex-grow-0 flex hover:opacity-80',
-                                            clearIcon: 'bg-multiselect-remove bg-center bg-no-repeat w-2.5 h-4 py-px box-content inline-block',
-                                            spinner: 'bg-multiselect-spinner bg-center bg-no-repeat w-4 h-4 z-10 mr-3.5 animate-spin flex-shrink-0 flex-grow-0',
-                                            dropdown: 'max-h-60 absolute -left-px -right-px bottom-0 transform translate-y-full border border-gray-300 -mt-px overflow-y-scroll z-50 bg-white flex flex-col rounded-b',
-                                            dropdownTop: '-translate-y-full top-px bottom-auto flex-col-reverse rounded-b-none rounded-t',
-                                            dropdownHidden: 'hidden',
-                                            options: 'flex flex-col p-0 m-0 list-none w-full',
-                                            optionsTop: 'flex-col-reverse',
-                                            group: 'p-0 m-0',
-                                            groupLabel: 'flex text-sm box-border items-center justify-start text-left py-2 px-3 font-semibold bg-gray-200 cursor-default leading-normal',
-                                            groupLabelPointable: 'cursor-pointer',
-                                            groupLabelPointed: 'bg-gray-300 text-black-700',
-                                            groupLabelSelected: 'bg-gray-100 text-black',
-                                            groupLabelSelectedPointed: 'bg-gray-100 text-black opacity-90',
-                                            groupOptions: 'p-0 m-0',
-                                            option: 'flex items-center justify-start box-border text-left cursor-pointer text-base leading-snug py-2 px-3',
-                                            optionPointed: 'text-gray-800 bg-gray-100',
-                                            optionSelected: 'text-white bg-indigo-500',
-                                            optionDisabled: 'text-gray-300 cursor-not-allowed',
-                                            optionSelectedPointed: 'text-white bg-indigo-500 opacity-90',
-                                            optionSelectedDisabled: 'text-green-100 bg-green-500 bg-opacity-50 cursor-not-allowed',
-                                            noOptions: 'py-2 px-3 text-gray-600 bg-white text-left',
-                                            noResults: 'py-2 px-3 text-gray-600 bg-white text-left',
-                                            fakeInput: 'bg-transparent absolute left-0 right-0 -bottom-px w-full h-px border-0 p-0 appearance-none outline-none text-transparent',
-                                            spacer: 'h-9 py-px box-content',
-                                        }"
-                                    /> -->
-                                </div>
                             </div>
 
                             <BreezeButton :route="route('product-categories.create')" @click="newAdmission(params.centre_id)">New Product Category</BreezeButton></div>
@@ -141,18 +82,6 @@ const handleDelete = (id) => {
                     </div>
                 </div>
             </div>
-            <!-- <ConfirmationModal
-                :show="isOpen"
-                @close="isOpen = false"
-                confirmationAlert="danger"
-                confirmationTitle="Delete Class"
-                confirmationText="Are you sure want to delete this class?"
-                confirmationButton="Delete"
-                confirmationMethod="delete"
-                :confirmationRoute="confirmationRoute"
-                :confirmationData="confirmationData"
-            >
-            </ConfirmationModal> -->
         </div>
     </BreezeAuthenticatedLayout>
 </template>
