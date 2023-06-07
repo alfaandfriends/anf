a27811df8262f78ebb7624ef95bfe4cc5bbeab2d
nazirul amin <lurizan23@gmail.com>
2023-06-07 10:49:39 +0800

    Setup product module

---
+++ resources/js/Pages/ProductCategory/Index.vue
@@ -0,0 +1,169 @@
+<script setup>
+import { ref, computed } from 'vue';
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import BreezeButton from '@/Components/Button.vue';
+import { Head, Link, usePage, useForm } from '@inertiajs/inertia-vue3';
+import ConfirmationModal from '@/Components/ConfirmationModal.vue';
+import Pagination from '@/Components/Pagination.vue';
+import { SearchIcon, TrashIcon, PencilIcon } from '@heroicons/vue/solid';
+import Multiselect from '@vueform/multiselect';
+import { $vfm, VueFinalModal, ModalsContainer } from 'vue-final-modal';
+
+const props = defineProps({
+    categories: {
+        type: Object,
+    },
+});
+</script>
+
+<template>
+    <Head title="Students" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header></template>
+        <div class="py-4 px-4">
+            <div class="overflow-x-auto">
+                <div class="mx-auto">
+                    <div class="align-middle inline-block min-w-full px-2">
+                        <div class="flex justify-between mb-4">
+                            <div class="flex space-x-2">
+                                <!-- <div class="flex relative text-gray-400 focus-within:text-gray-600">
+                                    <SearchIcon class="text-gray-600 h-4 w-4 fill-current pointer-events-none absolute top-1/4 left-3" :style="'top:30%'"></SearchIcon>
+                                    <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg focus:ring-0 focus:border-gray-300 appearance-none  block pl-10"
+                                            type="text" v-model="params.search" placeholder="Search">
+                                </div> -->
+                                <div class="flex">
+                                    <!-- <Multiselect
+                                        v-model="params.centre_id"
+                                        valueProp="ID"
+                                        :appendNewOption="false"
+                                        :searchable="true"
+                                        :options="$page.props.allowed_centres"
+                                        :clearOnSelect="false"
+                                        :canClear="false"
+                                        :canDeselect="false"
+                                        trackBy="label"
+                                        label="label"
+                                        :classes="{
+                                            container:
+                                                $page.props.errors.centre_id ?
+                                                'relative mx-auto w-full flex items-center justify-end box-border cursor-pointer border-2 border-red-300 rounded-lg bg-white text-base leading-snug outline-none h-10 px-40 pr-16 ':
+                                                'relative mx-auto w-full flex items-center justify-end box-border cursor-pointer border-2 border-gray-300 rounded-lg bg-white text-base leading-snug outline-none h-10 px-40 pr-16 ',
+                                            containerDisabled: 'cursor-default bg-gray-100',
+                                            containerOpen: 'rounded-b-none',
+                                            containerOpenTop: 'rounded-t-none',
+                                            containerActive: 'border border-gray-300',
+                                            singleLabel: 'flex items-center h-full max-w-full absolute left-0 top-0 pointer-events-none bg-transparent leading-snug pl-3.5 pr-16 box-border',
+                                            singleLabelText: 'overflow-ellipsis overflow-hidden block whitespace-nowrap max-w-full',
+                                            multipleLabel: 'flex items-center h-full absolute left-0 top-0 pointer-events-none bg-transparent leading-snug pl-3.5',
+                                            search: 'w-full absolute inset-0 outline-none focus:ring-0 appearance-none box-border border-0 text-base font-sans bg-white rounded pl-3.5',
+                                            placeholder: 'flex items-center h-full absolute left-0 top-0 pointer-events-none bg-transparent leading-snug pl-3.5 text-gray-500',
+                                            caret: 'bg-multiselect-caret bg-center bg-no-repeat w-2.5 h-4 py-px box-content mr-3.5 relative z-10 opacity-40 flex-shrink-0 flex-grow-0 transition-transform transform pointer-events-none',
+                                            caretOpen: 'rotate-180 pointer-events-auto',
+                                            clear: 'pr-3.5 relative z-10 opacity-40 transition duration-300 flex-shrink-0 flex-grow-0 flex hover:opacity-80',
+                                            clearIcon: 'bg-multiselect-remove bg-center bg-no-repeat w-2.5 h-4 py-px box-content inline-block',
+                                            spinner: 'bg-multiselect-spinner bg-center bg-no-repeat w-4 h-4 z-10 mr-3.5 animate-spin flex-shrink-0 flex-grow-0',
+                                            dropdown: 'max-h-60 absolute -left-px -right-px bottom-0 transform translate-y-full border border-gray-300 -mt-px overflow-y-scroll z-50 bg-white flex flex-col rounded-b',
+                                            dropdownTop: '-translate-y-full top-px bottom-auto flex-col-reverse rounded-b-none rounded-t',
+                                            dropdownHidden: 'hidden',
+                                            options: 'flex flex-col p-0 m-0 list-none w-full',
+                                            optionsTop: 'flex-col-reverse',
+                                            group: 'p-0 m-0',
+                                            groupLabel: 'flex text-sm box-border items-center justify-start text-left py-2 px-3 font-semibold bg-gray-200 cursor-default leading-normal',
+                                            groupLabelPointable: 'cursor-pointer',
+                                            groupLabelPointed: 'bg-gray-300 text-black-700',
+                                            groupLabelSelected: 'bg-gray-100 text-black',
+                                            groupLabelSelectedPointed: 'bg-gray-100 text-black opacity-90',
+                                            groupOptions: 'p-0 m-0',
+                                            option: 'flex items-center justify-start box-border text-left cursor-pointer text-base leading-snug py-2 px-3',
+                                            optionPointed: 'text-gray-800 bg-gray-100',
+                                            optionSelected: 'text-white bg-indigo-500',
+                                            optionDisabled: 'text-gray-300 cursor-not-allowed',
+                                            optionSelectedPointed: 'text-white bg-indigo-500 opacity-90',
+                                            optionSelectedDisabled: 'text-green-100 bg-green-500 bg-opacity-50 cursor-not-allowed',
+                                            noOptions: 'py-2 px-3 text-gray-600 bg-white text-left',
+                                            noResults: 'py-2 px-3 text-gray-600 bg-white text-left',
+                                            fakeInput: 'bg-transparent absolute left-0 right-0 -bottom-px w-full h-px border-0 p-0 appearance-none outline-none text-transparent',
+                                            spacer: 'h-9 py-px box-content',
+                                        }"
+                                    /> -->
+                                </div>
+                            </div>
+                            <BreezeButton @click="newAdmission(params.centre_id)">New Admission</BreezeButton>
+                        </div>
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-300">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/14">#</th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-3/14">Name</th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-2/14">Programme</th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-2/14">Parent</th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-2/14">Status</th>
+                                        <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider w-2/14">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    <tr v-if="!categories.length">
+                                        <td class="text-center" colspan="10">
+                                            <div class="p-3">
+                                                No Record Found!
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr class="hover:bg-gray-200" v-for="(students, index) in categories" :key="students.ID">
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-700">{{ ++index }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">{{ students.name }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">{{ students.programme_name }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">{{ students.parent_name }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full" :class="students.status == true ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'"> {{ students.status == true ? 'Active' : 'Not Active' }} </span>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                                            <div class="flex justify-center">
+                                                <div class="flex pr-1">
+                                                    <BreezeButton buttonType="warning" title="Edit Class" @click="editStudent(students.id)">
+                                                        <!-- <svg xmlns="http://www.w3.org/2000/svg" class="text-white-600 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                        </svg> -->
+                                                        Edit Student
+                                                    </BreezeButton>
+                                                </div>
+                                                <!-- <div class="flex">
+                                                    <BreezeButton buttonType="danger" title="Delete Class" @click="deleteStudent(students.id)">
+                                                        Delete
+                                                    </BreezeButton>
+                                                </div> -->
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <Pagination :page_data="categories"></Pagination>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- <ConfirmationModal
+                :show="isOpen"
+                @close="isOpen = false"
+                confirmationAlert="danger"
+                confirmationTitle="Delete Class"
+                confirmationText="Are you sure want to delete this class?"
+                confirmationButton="Delete"
+                confirmationMethod="delete"
+                :confirmationRoute="confirmationRoute"
+                :confirmationData="confirmationData"
+            >
+            </ConfirmationModal> -->
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
