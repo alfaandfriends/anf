7f74809efa99b7d5fa84a9a32103607b026696ee
nazirul amin <lurizan23@gmail.com>
2023-07-12 09:46:29 +0800

    Fix unable to upload after upload

--- resources/js/Components/UploadPreview.vue
+++ resources/js/Components/UploadPreview.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
         <div class="relative">
-            <input
-                type="file"
-                class="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-                @input="emit('update:value', $event.target.files[0])"
-            />
             <div v-if="previewUrl" class="relative w-24 h-24">
                 <img :src="previewUrl" alt="Preview" class="object-cover w-full h-full" />
             </div>
             <div v-else class="w-24 h-24 border-2 border-dashed border-gray-400 flex items-center justify-center">
                 <p class="text-gray-400">+</p>
             </div>
+            <input
+                type="file"
+                class="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                @input="emit('update:value', $event.target.files[0])"
+            />
         </div>
     </div>
 </template>
