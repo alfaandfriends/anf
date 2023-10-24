74e50389aa118ca37f9b40fc99de89cc7e25b0a6
Fijunas <abdulroaf628@gmail.com>
2023-10-24 12:28:03 +0800

    updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('invoice:generate')->everyFiveMinutes();
+        $schedule->command('invoice:generate')->cron('*/30 * * * *');
     }
 
     /**
