1344a156e84474f0e6555e5f3b4f8d7ed68177bf
Fijunas <abdulroaf628@gmail.com>
2023-10-24 11:21:08 +0800

    update cron schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('invoice:generate')->monthlyOn(1);
+        $schedule->command('invoice:generate')->everyTwoMinutes();
     }
 
     /**
