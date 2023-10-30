3d50f9941c3dc8e304f2adafdd10be39883f71de
Fijunas <abdulroaf628@gmail.com>
2023-10-30 09:06:30 +0800

    fix math manipulative upload and time

--- public/build/assets/ApplicationLogo-49b80978.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as a}from"./app-59b08c5f.js";const n=["href"],t={__name:"ApplicationLogo",setup(s){return(o,c)=>(e(),a("img",{href:o.route("dashboard"),src:"/images/anf-logo-main-2x.png",class:"w-35 h-35 block select-none",alt:""},null,8,n))}};export{t as _};
