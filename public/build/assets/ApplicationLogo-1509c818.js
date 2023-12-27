e9c9d1cccc3c190b025e0746d163d4355b20342b
Fijunas <abdulroaf628@gmail.com>
2023-12-27 23:16:27 +0800

    add console.log

--- public/build/assets/ApplicationLogo-1509c818.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as a}from"./app-535f0d6c.js";const n=["href"],t={__name:"ApplicationLogo",setup(s){return(o,c)=>(e(),a("img",{href:o.route("dashboard"),src:"/images/anf-logo-main-2x.png",class:"block select-none",alt:""},null,8,n))}};export{t as _};
