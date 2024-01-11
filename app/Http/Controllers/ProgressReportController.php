7984702d51df8d651d0449265225643767031614
Fijunas <abdulroaf628@gmail.com>
2024-01-11 10:58:34 +0800

    change progress report order by date

--- app/Http/Controllers/ProgressReportController.php
+++ app/Http/Controllers/ProgressReportController.php
@@ -112,7 +112,7 @@ class ProgressReportController extends Controller
                                     ->select('progress_report_details.progress_report_id', 'progress_report_details.id', 'progress_report_details.date', 
                                             'progress_report_details.report_data', 'progress_report_details.comments', 'progress_report_details.attendance_status',
                                             'progress_report_status.class as attendance_status_class_name', 'progress_report_status.name as attendance_status_name')
-                                    ->where('progress_report_id', $request->progress_report_id)->get();
+                                    ->where('progress_report_id', $request->progress_report_id)->orderBy('progress_report_details.date')->get();
         
         return Inertia::render('ProgressReport/Templates/'.$config_info->vue_template, [
             'student_info'          => $student_info,
