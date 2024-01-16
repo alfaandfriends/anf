6242852dc5223959f4e3cc11418d0c0f5aaa743a
Fijunas <abdulroaf628@gmail.com>
2024-01-16 17:03:39 +0800

    compile

--- public/build/assets/ApplicationLogo-d3f3c450.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as a}from"./app-41239a54.js";const n=["href"],t={__name:"ApplicationLogo",setup(s){return(o,c)=>(e(),a("img",{href:o.route("dashboard"),src:"/images/anf-logo-main-2x.png",class:"block select-none",alt:""},null,8,n))}};export{t as _};
