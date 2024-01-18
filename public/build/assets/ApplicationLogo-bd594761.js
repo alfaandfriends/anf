049a7ab14c261dcfaed4c3bec2fb159d9e9748d8
Fijunas <abdulroaf628@gmail.com>
2024-01-18 16:36:51 +0800

    temporary disable artbook

---
+++ public/build/assets/ApplicationLogo-bd594761.js
@@ -0,0 +1,1 @@
+import{o as e,c as a}from"./app-61ed3a80.js";const n=["href"],t={__name:"ApplicationLogo",setup(s){return(o,c)=>(e(),a("img",{href:o.route("dashboard"),src:"/images/anf-logo-main-2x.png",class:"block select-none",alt:""},null,8,n))}};export{t as _};
