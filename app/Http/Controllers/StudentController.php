47588ec7ba6f9e9a91cc7fea4ed5b032bdb4cc09
Fijunas <abdulroaf628@gmail.com>
2024-01-19 16:11:04 +0800

    fix parent's child default

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -630,10 +630,10 @@ class StudentController extends Controller
             }
         })->toArray();
 
-        Log::info('centre_info: '.$centre_info);
-        Log::info('fee_info: '.$fee_info);
-        Log::info('invoice_info: '.$invoice_info);
-        Log::info('new_invoice_items: '.$new_invoice_items);
+        Log::info($centre_info);
+        Log::info($fee_info);
+        Log::info($invoice_info);
+        Log::info($new_invoice_items);
         
         DB::table('student_fees')->where('student_id', $request->student_id)->where('id', $request->student_fee_id)->update([
             'centre_id'    => $request->centre_id,
