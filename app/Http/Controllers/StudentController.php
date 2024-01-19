6050aa5f1f76c332e10a7937ffbe249887ab9877
Fijunas <abdulroaf628@gmail.com>
2024-01-19 16:13:50 +0800

    fix parent's child default

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -631,7 +631,7 @@ class StudentController extends Controller
         })->toArray();
 
         Log::info($centre_info);
-        Log::info($fee_info->toArray());
+        Log::info($fee_info ? $fee_info->toArray() : 'No fee information found.');
         Log::info($invoice_info);
         Log::info($new_invoice_items);
         
