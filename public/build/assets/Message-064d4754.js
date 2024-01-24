7027e5ed7465ce0f2bf9d34cb681d9dcb617d577
Fijunas <abdulroaf628@gmail.com>
2024-01-24 08:59:36 +0800

    add generate progress report using dompdf

---
+++ public/build/assets/Message-064d4754.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as s,c as t,d as o}from"./app-4d609b65.js";const c={},a={class:"bg-gray-100"},r=o("div",{class:"container text-gray-800 grid justify-center grid-cols-5 gap-8 px-48 py-5 mx-auto"},"Message",-1),n=[r];function _(i,d){return s(),t("div",a,n)}const p=e(c,[["render",_]]);export{p as default};
