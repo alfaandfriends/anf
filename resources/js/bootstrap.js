233c578429b3091b9bca56f3aa41349aaea88909
Fijunas <abdulraof628@gmail.com>
2024-04-29 16:49:46 +0800

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,7 +26,7 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: '103.27.73.60',
+    wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT,
     wssPort: import.meta.env.VITE_REVERB_PORT,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
