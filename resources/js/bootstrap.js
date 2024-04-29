de0dc8e97ae4eadce39aed216c7e1161e8a7c9bf
Fijunas <abdulraof628@gmail.com>
2024-04-29 16:21:20 +0800

    change vite echos

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,7 +26,7 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsHost: '127.0.0.1',
     wsPort: import.meta.env.VITE_REVERB_PORT,
     wssPort: import.meta.env.VITE_REVERB_PORT,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
