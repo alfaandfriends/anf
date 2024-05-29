87007d216d4d5a4ab0f8b5e1760c35738ef554ac
Fijunas <abdulraof628@gmail.com>
2024-05-29 14:43:17 +0800

    fix chart responsiveness

--- resources/js/Pages/CRM/Dashboard.vue
+++ resources/js/Pages/CRM/Dashboard.vue
@@ -5,8 +5,9 @@ import BreezeAuthenticatedLayout from '@/Layouts/CRM/Authenticated.vue';
 
 <template>
     <BreezeAuthenticatedLayout>
-        <div class="grid grid-cols-4">
+        <div class="grid grid-cols-1 lg:grid-cols-4">
             <div class="col-span-1">
+                <label for="">Demo Chart</label>
                 <canvas id="bar-chart"></canvas>
             </div>
         </div>
