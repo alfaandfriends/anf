875f34b011fe2d78b6afacfdb82629678625de6c
Fijunas <abdulraof628@gmail.com>
2024-10-03 11:50:12 +0800

    fix datepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 // import '../css/app.css';
 import '@vueform/toggle/themes/default.css'
 import '@vueform/multiselect/themes/default.css'
-// import '@vuepic/vue-datepicker/dist/main.css'
+import '@vuepic/vue-datepicker/dist/main.css'
 import 'cropperjs/dist/cropper.js'
 import 'cropperjs/dist/cropper.css'
 
