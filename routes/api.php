71397c0b42947d4428dc3b2b504043d4ca148675
Fijunas <abdulraof628@gmail.com>
2024-11-07 14:49:07 +0800

    add auth route

--- routes/api.php
+++ routes/api.php
@@ -26,20 +26,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('find-users', [UserController::class, 'getUsers'])->name('users.find');
-Route::get('find-email/{email}', [UserController::class, 'emailExist']);
-Route::get('find-username/{username}', [UserController::class, 'usernameExist']);
-Route::get('find-products', [ProductHelper::class, 'getProducts'])->name('products.find_products');
-Route::get('find-product-variations', [ProductHelper::class, 'getProductVariations'])->name('products.find_product_variations');
-Route::get('find-product-sub-variations', [ProductHelper::class, 'getProductSubVariations'])->name('products.find_product_sub_variations');
+Route::middleware(['auth'])->group(function(){
+    Route::get('find-users', [UserController::class, 'getUsers'])->name('users.find');
+    Route::get('find-email/{email}', [UserController::class, 'emailExist']);
+    Route::get('find-username/{username}', [UserController::class, 'usernameExist']);
+    Route::get('find-products', [ProductHelper::class, 'getProducts'])->name('products.find_products');
+    Route::get('find-product-variations', [ProductHelper::class, 'getProductVariations'])->name('products.find_product_variations');
+    Route::get('find-product-sub-variations', [ProductHelper::class, 'getProductSubVariations'])->name('products.find_product_sub_variations');
 
-Route::post('fee_invoices/callback/my', [InvoiceController::class, 'callbackMy'])->name('fee.invoices.callback.my');
-Route::any('fee_invoices/callback/id', [InvoiceController::class, 'callbackId'])->name('fee.invoices.callback.id');
-Route::get('fee_invoices/check-status', [InvoiceController::class, 'checkStatus'])->name('fee.invoices.check_status');
-Route::get('/fee_invoices/generate', [InvoiceController::class, 'feeInvoiceGenerate'])->name('fee.invoices.generate');
+    Route::post('fee_invoices/callback/my', [InvoiceController::class, 'callbackMy'])->name('fee.invoices.callback.my');
+    Route::any('fee_invoices/callback/id', [InvoiceController::class, 'callbackId'])->name('fee.invoices.callback.id');
+    Route::get('fee_invoices/check-status', [InvoiceController::class, 'checkStatus'])->name('fee.invoices.check_status');
+    Route::get('/fee_invoices/generate', [InvoiceController::class, 'feeInvoiceGenerate'])->name('fee.invoices.generate');
 
-Route::get('/programmes/get-fee', [ProgrammeController::class, 'getFee'])->name('programmes.get_fee');
+    Route::get('/programmes/get-fee', [ProgrammeController::class, 'getFee'])->name('programmes.get_fee');
 
-Route::get('classes/get-class-types', [ClassController::class, 'getClassTypes'])->name('classes.get_class_types');
-Route::get('classes/get-class-levels', [ClassController::class, 'getClassLevels'])->name('classes.get_class_levels');
-Route::get('classes/find', [ClassController::class, 'findClasses'])->name('classes.find');
+    Route::get('classes/get-class-types', [ClassController::class, 'getClassTypes'])->name('classes.get_class_types');
+    Route::get('classes/get-class-levels', [ClassController::class, 'getClassLevels'])->name('classes.get_class_levels');
+    Route::get('classes/find', [ClassController::class, 'findClasses'])->name('classes.find');
+});
