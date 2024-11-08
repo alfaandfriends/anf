c3eb4de5239b7603512bec9580a25be835c06a1f
Fijunas <abdulraof628@gmail.com>
2024-11-08 16:19:13 +0800

    WIP

--- public/build/assets/AlertTitle-eeef0217.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./app-f7a369e6.js";import{t as r,y as n,B as l,q as o,u as c}from"./@vue-d42dd214.js";const u={__name:"AlertTitle",props:{class:{type:null,required:!1}},setup(e){const s=e;return(t,m)=>(r(),n("h5",{class:o(c(a)("mb-1 font-medium leading-none tracking-tight",s.class))},[l(t.$slots,"default")],2))}};export{u as _};
--- resources/js/Components/Chatbox.vue
+++ resources/js/Components/Chatbox.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 import { BotMessageSquareIcon, Send, X } from 'lucide-vue-next';
+import { VMarkdownView } from 'vue3-markdown'
+import 'vue3-markdown/dist/style.css'
 </script>
 
 <script>
@@ -14,13 +16,18 @@ export default {
             user_input: '',
             chatCompletion: '',
             response: '',
+            post_prompt: `
+                Please greet the user by name and remain focused on lesson-related inquiries only, 
+                Only respond to questions related directly to the lesson plan topic or content,
+                Do not answer any unrelated questions. Do not solve math problem. If an unrelated question is asked, remind the user, "I am here to assist with lesson plan questions only."
+            `
+            ,
             conversation: [{ 
                 role:   'system', 
                 content:`
-                            You are AI assistant, greet user ${this.$page.props.auth.user.display_name}. 
-                            If have spacing and keep everything strictly in context.
-                            Puchong, Malaysia based location.
-                        ` 
+                    You are an AI assistant dedicated solely to answering questions related to the current lesson plan. 
+                    Your task is to help ${this.$page.props.auth.user.display_name} by providing clear, concise information that is relevant only to the lesson plan topic.
+                `
             }]
         }
     },
@@ -36,6 +43,9 @@ export default {
             this.conversation.push({
                 role: 'user', 
                 content: this.user_input
+            },{
+                role: 'system', 
+                content: this.post_prompt
             });
 
             this.user_input = '';
@@ -47,10 +57,11 @@ export default {
 
             this.chatCompletion = await this.client.chat.completions.create({
                 messages: this.conversation,
-                model: 'llama3-8b-8192',
-                temperature: 0.2,
-                max_tokens: 1200,
-                top_p: 0.5,
+                frequency_penalty: 1.0,
+                model: 'llama-3.1-70b-versatile',
+                temperature: 0.4,
+                max_tokens: 1024,
+                top_p: 1,
                 stream: true,
             });
 
@@ -78,11 +89,10 @@ export default {
     <div class="fixed bottom-5 right-5 z-50">
         <div v-if="open" class="flex flex-col bg-background border sm:rounded-lg shadow-md overflow-hidden transition-all duration-250 ease-out sm:absolute sm:w-[90vw] sm:h-[80vh] fixed inset-0 w-full h-full sm:inset-auto sm:bottom-[calc(100%+10px)] sm:right-0 sm:max-w-lg sm:max-h-[700px] pointer-events-auto opacity-100 visible scale-100 translate-y-0">
             
-            <!-- Header -->
             <div class="flex items-center p-4 border-b flex-col text-center justify-center">
                 <h1 class="text-base font-semibold">ALFA and Friends AI ✨</h1>
                 <p class="text-xs">Ask any question for our AI to answer</p>
-                <span class="md:hidden fixed top-2 right-2"><X/></span>
+                <span class="md:hidden fixed top-6 right-4 cursor-pointer" @click="open = false"><X/></span>
                 
                 <!-- Buttons -->
                 <!-- <div class="flex gap-2 items-center pt-2">
@@ -95,18 +105,22 @@ export default {
                 </div> -->
             </div>
 
-            <!-- Chat Messages -->
             <div class="flex-grow overflow-y-auto text-sm">
-                <div class="flex flex-col w-full h-full p-4 gap-6 overflow-y-auto dark:bg-muted/40" ref="chatbox">
+                <div class="flex flex-col h-full p-4 gap-6 overflow-y-auto dark:bg-muted/40" ref="chatbox">
                     <div class="flex flex-col gap-2" style="opacity: 1; transform: none; transform-origin: 50% 50% 0px;" v-if="conversation.length > 1" v-for="chat in conversation">
-                        <div class="flex gap-2 items-end group self-start relative group" v-if="chat.role == 'assistant'">
-                            <span class="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full">
-                                <span class="flex h-full text-base w-full items-center justify-center rounded-full bg-muted"><BotMessageSquareIcon/></span>
+                        <div class="flex items-start group self-start relative group" v-if="chat.role == 'assistant'">
+                            <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-full">
+                                <span class="flex h-full text-base w-full items-center justify-center rounded-full bg-muted"><BotMessageSquareIcon class="h-4 w-4"/></span>
                             </span>
-                            <div class="p-4 bg-secondary text-secondary-foreground rounded-r-lg rounded-tl-lg break-words max-w-full" v-html="chat.content"></div>
+                            <div class="px-4 text-secondary-foreground rounded-r-lg rounded-tl-lg break-words w-full">
+                                <VMarkdownView
+                                    mode="light"
+                                    :content="chat.content"
+                                ></VMarkdownView>
+                            </div>
                         </div>
                         <div class="flex justify-end" v-if="chat.role == 'user'">
-                            <div class="p-4 border text-secondary-foreground rounded-r-lg rounded-tl-lg break-words max-w-full ml-10">
+                            <div class="p-4 border border-slate-400 text-secondary-foreground rounded-l-lg rounded-tr-lg break-words max-w-full ml-10">
                                 {{ chat.content }}
                             </div>
                         </div>
@@ -114,7 +128,6 @@ export default {
                 </div>
             </div>
 
-            <!-- Message Input -->
             <form @submit.prevent="submit">
                 <div class="flex items-center gap-2 border-t p-4">
                     <div class="flex-grow">
@@ -125,10 +138,9 @@ export default {
             </form>
         </div>
 
-        <!-- Main Close Button -->
-         <Button class="flex items-center space-x-1" @click="open = !open">
+        <Button class="flex items-center space-x-1" @click="open = !open">
             <BotMessageSquareIcon/>
             <span>AI Assistant</span>
-         </Button>
+        </Button>
     </div>
 </template>
