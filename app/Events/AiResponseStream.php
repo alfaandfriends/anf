b15fce235fd4b446e785dfeb46ab7b026a7b02b9
Fijunas <abdulraof628@gmail.com>
2024-12-31 14:15:32 +0800

    WIP

--- app/Events/AiResponseStream.php
+++ app/Events/AiResponseStream.php
@@ -45,9 +45,4 @@ class AiResponseStream implements ShouldBroadcastNow
     {
         return [$this->message];
     }
-
-    public function broadcastQueue(): string
-    {
-        return 'ai_stream_response';
-    }
 }
