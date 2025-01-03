e97f092916c8c345f1ccab0914d6a7813b31ca95
Fijunas <abdulraof628@gmail.com>
2025-01-03 17:19:05 +0800

    fixed receipt

--- app/Http/Controllers/Parent/InvoiceController.php
+++ app/Http/Controllers/Parent/InvoiceController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Parent;
 
 use App\Classes\InvoiceHelper;
 use App\Http\Controllers\Controller;
-use Billplz;
+use Billplz\Laravel\Billplz;
 use DB;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
